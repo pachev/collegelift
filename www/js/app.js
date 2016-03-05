b21cdde483597c93068d8b31562a78c18935b8a1
eliecer alejandro <eliman341@gmail.com>
2016-03-05 17:10:45 -0500

    First Commit

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
+angular.module("starter", ["ionic", "firebase"])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +23,68 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+
+var exampleApp = angular.module('starter', ['ionic', 'firebase']);
+var fb = null;
+ 
+exampleApp.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+        fb = new Firebase("https://example.firebaseio.com/");
+    });
+});
+
+$scope.list = function() {
+    fbAuth = fb.getAuth();
+    if(fbAuth) {
+        var syncObject = $firebaseObject(fb.child("users/" + fbAuth.uid));
+        syncObject.$bindTo($scope, "data");
+    }
+}
+
+exampleApp.controller("TodoController", function($scope, $firebaseObject, $ionicPopup) {
+ 
+    $scope.list = function() {
+ 
+    }
+ 
+    $scope.create = function() {
+ 
+    }
+ 
+});
+exampleApp.controller("LoginController", function($scope, $firebaseAuth, $location) {
+ 
+    $scope.login = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$authWithPassword({
+            email: username,
+            password: password
+        }).then(function(authData) {
+            $location.path("/todo");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    }
+ 
+    $scope.register = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$createUser({email: username, password: password}).then(function() {
+            return fbAuth.$authWithPassword({
+                email: username,
+                password: password
+            });
+        }).then(function(authData) {
+            $location.path("/todo");
+        }).catch(function(error) {
+            console.error("ERROR " + error);
+        });
+    }
+ 
+});
