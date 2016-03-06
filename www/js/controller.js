5adcf4666a05d2931b1bcb3c8ee6efa2a33de29f
eliecer alejandro <eliman341@gmail.com>
2016-03-06 07:04:56 -0500

    Last one

--- www/js/controller.js
+++ www/js/controller.js
@@ -4,23 +4,3 @@ function ContentController($scope, $ionicSideMenuDelegate) {
   };
 }
 
-$(function () {
-	$('.navbar-minimal a').on('click', function() { $('.navbar-toggler').trigger('click'); });
-    /* START OF DEMO JS - NOT NEEDED */
-    if (window.location == window.parent.location) {
-        $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
-        $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
-        $('#fullscreen').attr('title', 'Back To Bootsnipp');
-    }    
-    $('#fullscreen').on('click', function(event) {
-        event.preventDefault();
-        window.parent.location =  $('#fullscreen').attr('href');
-    });
-    $('#fullscreen').tooltip();
-    /* END DEMO OF JS */
-    
-    $('.navbar-toggler').on('click', function(event) {
-		event.preventDefault();
-		$(this).closest('.navbar-minimal').toggleClass('open');
-	})
-});
