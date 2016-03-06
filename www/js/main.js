f0cfe5750f605b1a956c8a2a3a71abe28fc14573
eliecer alejandro <eliman341@gmail.com>
2016-03-05 19:46:31 -0500

    Second

--- www/js/main.js
+++ www/js/main.js
@@ -9,6 +9,10 @@ function validate(){
     window.location = "DriverUser.html"; // Redirecting to other page.
     return false;
     }
+    if ( username == "hey@fiu.edu" && password == "1234"){
+    window.location = "loginUser.html"; // Redirecting to other page.
+    return false;
+    }
     else{
     attempt --;// Decrementing by one.
     alert("You have left "+attempt+" attempt;");
@@ -21,6 +25,113 @@ function validate(){
         }
     }
 }
+
+var $form = $('#payment-form');
+$form.on('submit', payWithStripe);
+
+/* If you're using Stripe for payments */
+function payWithStripe(e) {
+    e.preventDefault();
+
+    /* Visual feedback */
+    $form.find('[type=submit]').html('Validating <i class="fa fa-spinner fa-pulse"></i>');
+
+    var PublishableKey = 'pk_test_6pRNASCoBOKtIshFeQd4XMUh'; // Replace with your API publishable key
+    Stripe.setPublishableKey(PublishableKey);
+    Stripe.card.createToken($form, function stripeResponseHandler(status, response) {
+        console.log(response);
+        if (response.error) {
+            /* Visual feedback */
+            $form.find('[type=submit]').html('Try again');
+            /* Show Stripe errors on the form */
+            $form.find('.payment-errors').text(response.error.message);
+            $form.find('.payment-errors').closest('.row').show();
+        } else {
+            /* Visual feedback */
+            $form.find('[type=submit]').html('Processing <i class="fa fa-spinner fa-pulse"></i>');
+            /* Hide Stripe errors on the form */
+            $form.find('.payment-errors').closest('.row').hide();
+            $form.find('.payment-errors').text("");
+            // response contains id and card, which contains additional card details
+            var token = response.id;
+            console.log(token);
+            // AJAX
+            $.post('/account/stripe_card_token', {
+                    token: token
+                })
+                // Assign handlers immediately after making the request,
+                .done(function(data, textStatus, jqXHR) {
+                    $form.find('[type=submit]').html('Payment successful <i class="fa fa-check"></i>').prop('disabled', true);
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    $form.find('[type=submit]').html('There was a problem').removeClass('success').addClass('error');
+                    /* Show Stripe errors on the form */
+                    $form.find('.payment-errors').text('Try refreshing the page and trying again.');
+                    $form.find('.payment-errors').closest('.row').show();
+                });
+        }
+    });
+}
+
+/* Form validation */
+jQuery.validator.addMethod("month", function(value, element) {
+  return this.optional(element) || /^(01|02|03|04|05|06|07|08|09|10|11|12)$/.test(value);
+}, "Please specify a valid 2-digit month.");
+
+jQuery.validator.addMethod("year", function(value, element) {
+  return this.optional(element) || /^[0-9]{2}$/.test(value);
+}, "Please specify a valid 2-digit year.");
+
+validator = $form.validate({
+    rules: {
+        cardNumber: {
+            required: true,
+            creditcard: true,
+            digits: true
+        },
+        expMonth: {
+            required: true,
+            month: true
+        },
+        expYear: {
+            required: true,
+            year: true
+        },
+        cvCode: {
+            required: true,
+            digits: true
+        }
+    },
+    highlight: function(element) {
+        $(element).closest('.form-control').removeClass('success').addClass('error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-control').removeClass('error').addClass('success');
+    },
+    errorPlacement: function(error, element) {
+        $(element).closest('.form-group').append(error);
+    }
+});
+
+paymentFormReady = function() {
+    if ($form.find('[name=cardNumber]').hasClass("success") &&
+        $form.find('[name=expMonth]').hasClass("success") &&
+        $form.find('[name=expYear]').hasClass("success") &&
+        $form.find('[name=cvCode]').val().length > 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$form.find('[type=submit]').prop('disabled', true);
+var readyInterval = setInterval(function() {
+    if (paymentFormReady()) {
+        $form.find('[type=submit]').prop('disabled', false);
+        clearInterval(readyInterval);
+    }
+}, 250);
+
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 
